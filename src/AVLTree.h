d3e9edc4bf76d68c8c935b4fe090a4b74052a5f2
Mya <mya.ramsey@ufl.edu>
2024-02-09 20:19:36 -0500

    - Finished checking valid name and id function. - Plan to figure out height function, input commands, and name checking twice.

--- src/AVLTree.h
+++ src/AVLTree.h
@@ -33,26 +33,47 @@ public:
 };
 
    bool isValidName(string name){
-       for(int i =0; i < name.size(); i++){
+       for(unsigned int i = 0; i < name.length(); i++){
            if(isdigit(name[i]) || ispunct(name[i])){
                return false;
            }
        }
        return true;
    }
+   bool isValidID(string id){
+       for(unsigned int i = 0; i < id.length(); i++){
+           if(!isdigit(id[i])){
+              return false;
+           }
+       }
+       return true;
+   }
+
+   //TODO: Add height update function to the tree so we can update and figure out when we can rotate the tree
+
     AVLNode* insert(AVLNode* root, string name, string id){
         // check to see of name and id are correct
+        //TODO: Figure out why is it checking the name twice.
+        //TODO: Ask Zach about the command line and command stuff <3
+        if(isValidName(name) && isValidID(id)){
+            cout << name << " " << id << " is valid name and id" << endl;
+            if(root == nullptr){
+                return new AVLNode(name,id);
+            }
+            else if(id < root->studentID){
+                root->left = insert(root->left,name,id);
+            }
+            else if(id > root->studentID){
+                root->right = insert(root->right,name,id);
+            }
+            return root;
+        }
+        else{
+            cout << name <<" " << id <<  " is not valid name or id" << endl;
+            return root;
+        }
 
-        if(root == nullptr){
-            return new AVLNode(name,id);
-        }
-        else if(id < root->studentID){
-            root->left = insert(root->left,name,id);
-        }
-        else if(id > root->studentID){
-            root->right = insert(root->right,name,id);
-        }
-        return root;
+
     }
 
     void printAVLTree(AVLNode* root){
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 #include "AVLTree.h"
 
@@ -12,6 +11,9 @@ int main(){
     root = insert(root,"Mya","30981303");
     root = insert(root,"Emmy","102345");
     root = insert(root,"Zach","88829201");
+    root = insert(root,"9s9sd","009101");
+    root = insert(root,"Cielo","2921a");
+    root = insert(root,"sdjs/","1890218");
     printAVLTree(root);
 
 
